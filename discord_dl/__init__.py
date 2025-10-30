66156ebafbeff0c3af58857455609aa89fcb89a3
8cr8rs <eightcraters@hotmail.com>
2025-10-30 18:00:24 +0700

    Creating entrypoints.

--- discord_dl/__init__.py
+++ discord_dl/__init__.py
@@ -1,1 +1,6 @@
+import sys
 
+__license__ = "The Unlicense"
+
+def main(argv = None):
+  sys.exit(0)
--- discord_dl/__main__.py
+++ discord_dl/__main__.py
@@ -1,3 +1,14 @@
-# Main File
+import sys
 
-print("Hello World!")
+# Call at repository's root directory using:
+# python -m discord_dl
+
+# if (__package__ == None) and (not getattr(sys, "frozen", False)):
+  # import os.path
+
+import discord_dl
+
+# Main functionalities will only run if the file is called directly.
+if (__name__ == "__main__"):
+  print("Running from file: " + __file__)
+  discord_dl.main()
